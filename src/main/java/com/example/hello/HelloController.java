c1875688e7bef2e67cdc0f71e3a09710bd8930c3
s2peachy <omgmilzy@gmail.com>
2022-09-27 02:44:07 +0900

    3.3Webアプリケーションの概要_動作確認

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+	
+	@Autowired
+	private HelloService service;
 
 	@GetMapping("/hello")
 	public String getHello() {
@@ -15,14 +19,17 @@ public class HelloController {
 		return "hello";
 	}
 	
-	@PostMapping("/hello")
-	public String postRequest(@RequestParam("text1")String str, Model model) {
+	@PostMapping("/hello/db")
+	public String postDbRequest(@RequestParam("text2")String id, Model model) {
 		
-		//画面から受け取った文字列をmodelに登録
-		model.addAttribute("sample", str);
+		//1件検索
+		Employee employee = service.getEmployee(id);
 		
-		//response.htmlに画面遷移
-		return "hello/response";
+		//検索結果をModelに登録
+		model.addAttribute("employee", employee);
+		
+		//db.htmlに画面遷移
+		return "hello/db";
 	}
 }
 
