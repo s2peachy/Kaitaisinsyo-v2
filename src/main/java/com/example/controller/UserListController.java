c7dc5b8b357aa6b63532cb75c332e6446d5bb70e
s2peachy <omgmilzy@gmail.com>
2022-10-06 14:48:00 +0900

    8.3.1動的SQL_動作確認

--- src/main/java/com/example/controller/UserListController.java
+++ src/main/java/com/example/controller/UserListController.java
@@ -3,14 +3,18 @@ package com.example.controller;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.user.model.MUser;
 import com.example.domain.user.service.UserService;
+import com.example.form.UserListForm;
 
 @Controller
 @RequestMapping("/user")
@@ -19,12 +23,18 @@ public class UserListController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	/**ユーザー一覧画面を表示*/
 	@GetMapping("/list")
-	public String getUserList(Model model) {
+	public String getUserList(@ModelAttribute UserListForm form, Model model) {
 		
-		//ユーザー一覧取得
-		List<MUser> userList = userService.getUsers();
+		//formをMUserクラスに変換
+		MUser user = modelMapper.map(form, MUser.class);
+		
+		//ユーザー検索
+		List<MUser> userList = userService.getUsers(user);
 		
 		//Modelに登録
 		model.addAttribute("userList", userList);
@@ -33,6 +43,21 @@ public class UserListController {
 		return "user/list";
 	}
 	
-
+	/**ユーザー検索処理*/
+	@PostMapping("/list")
+	public String postUserList(@ModelAttribute UserListForm form, Model model) {
+		
+		//formをMUserクラスに変換
+		MUser user = modelMapper.map(form, MUser.class);
+		
+		//ユーザー検索
+		List<MUser> userList = userService.getUsers(user);
+		
+		//Modelに登録
+		model.addAttribute("userList", userList);
+		
+		//ユーザー一覧画面を表示
+		return "user/list";
+	}
 
 }
