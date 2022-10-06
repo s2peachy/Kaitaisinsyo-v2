835c61f03f2dce972867e58f7c77b97c7ae32fd5
s2peachy <omgmilzy@gmail.com>
2022-10-06 15:03:10 +0900

    8.3.2ネストしたオブジェクトへのマッピング_動作確認

--- src/main/java/com/example/form/UserDetailForm.java
+++ src/main/java/com/example/form/UserDetailForm.java
@@ -2,6 +2,8 @@ package com.example.form;
 
 import java.util.Date;
 
+import com.example.domain.user.model.Department;
+
 import lombok.Data;
 
 @Data
@@ -12,5 +14,6 @@ public class UserDetailForm {
 	private Date birthday;
 	private Integer age;
 	private Integer gender;
+	private Department department;
 
 }
